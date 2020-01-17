35534f32ef33b17879fac7ad01a858c50b34e17b
Buu Kidd <quantrithanh1999@gmail.com>
2020-01-17 16:09:19 -0500

    upload small stuff

--- Mobile_version/src/app/app.module.ts
+++ Mobile_version/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { NativeScriptFormsModule  } from "nativescript-angular/forms"
+import { NativeScriptUICalendarModule } from "nativescript-ui-calendar/angular";
 
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -40,7 +41,8 @@ import { EmployeePayrollComponent } from './employee/employee-payroll/employee-p
         NativeScriptModule,
         AppRoutingModule,
         NativeScriptRouterModule,
-        NativeScriptFormsModule
+        NativeScriptFormsModule,
+        NativeScriptUICalendarModule
     ],
     declarations: [
 
