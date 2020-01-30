9c26aa4b28d4dbc29f8bb5b5047dd61db61c98f7
Thong Nguyen <bathong170698@gmail.com>
2020-01-30 13:48:28 -0500

    Style changed

--- BusinessWeb/src/app/mainlanding/mainlanding.component.ts
+++ BusinessWeb/src/app/mainlanding/mainlanding.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-mainlanding',
@@ -12,4 +12,5 @@ export class MainlandingComponent implements OnInit {
   ngOnInit() {
   }
 
+  
 }
