5adb81ddac43bb5ec3a9e4b7967c753391622677
Thong Nguyen <bathong170698@gmail.com>
2020-02-03 20:21:11 -0500

    Modified app-routing.module and app.module

--- business-web/src/app/app.module.ts
+++ business-web/src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 
-import {MatMenuModule,MatToolbarModule,MatGridListModule, MatGridList,MatButtonModule,MatFormFieldModule, MatInputModule} from '@angular/material';
+
+import {MatMenuModule,MatToolbarModule,MatGridListModule, MatGridList,MatButtonModule,MatFormFieldModule, MatInputModule,MatIconModule, MatIcon} from '@angular/material';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainlandingComponent } from './mainlanding/mainlanding.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { InventoryComponent } from './inventory/inventory.component';
-import { ScheduleComponent } from './schedule/schedule.component';
-import { PaystubComponent } from './paystub/paystub.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
+import { MainlandingComponent } from './features/mainlanding/mainlanding.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { InventoryComponent } from './features/inventory/inventory.component';
+import { ScheduleComponent } from './features/schedule/schedule.component';
+import { PaystubComponent } from './features/paystub/paystub.component';
+import { ContactUsComponent } from './features/contact-us/contact-us.component';
+import { NavbarComponent } from './features/navbar/navbar.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { TrialPageComponent } from './trial-page/trial-page.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +29,10 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
     InventoryComponent,
     ScheduleComponent,
     PaystubComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    NavbarComponent,
+    MainPageComponent,
+    TrialPageComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +44,9 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    NgbModule
+    NgbModule,
+    MatIconModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
