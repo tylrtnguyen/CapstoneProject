7e746a9530b2a00b9c9e92e10d978c32185309c6
Vo Anh Tu Nguyen <nguyenvoanhtu129@gmail.com>
2020-02-12 17:42:20 -0500

    Employee profile

--- Mobile_version/src/app/employee/employee-profile/employee-profile.component.ts
+++ Mobile_version/src/app/employee/employee-profile/employee-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as dialogs from "tns-core-modules/ui/dialogs";
+
 
 @Component({
   selector: 'ns-employee-profile',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-profile.component.css']
 })
 export class EmployeeProfileComponent implements OnInit {
+  id = '123456'
+  name = 'Tu Nguyen'
+  email = 'nvatu129@test.test'
+  phone = '(647) 746 7746'
+  location = 'Chew & Chill (Dundas)'
+  department = 'FOH'
 
   constructor() { }
 
+
   ngOnInit() {
+  }
+  changeEmail() {
+    dialogs.prompt({
+      title: "Change Email",
+      message: "Please enter your new email address",
+      okButtonText: "Confirm",
+      cancelButtonText: "Cancel",
+      defaultText: this.email,
+      inputType: dialogs.inputType.email
+    }).then((r) => {
+      if(r.result === true) this.email = r.text;
+    })
+  }
+  changePhone() {
+    dialogs.prompt({
+      title: "Change Phone Number",
+      message: "Please enter your new phone number",
+      okButtonText: "Confirm",
+      cancelButtonText: "Cancel",
+      defaultText: this.phone,
+      inputType: dialogs.inputType.phone
+    }).then((r) => {
+      if(r.result === true) this.phone = r.text;
+    })
   }
 
 }
